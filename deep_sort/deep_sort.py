d0161fe26c7974c21d18b4a9203014d485f43f94
mikel.brostrom <mikel.brostrom@cevt.se>
2020-02-05 11:33:54 +0100

    deep_sort.py cleanup

--- deep_sort/deep_sort.py
+++ deep_sort/deep_sort.py
@@ -46,13 +46,12 @@ class DeepSort(object):
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
             box = track.to_tlwh()
-            x1,y1,x2,y2 = self._tlwh_to_xyxy(box)
+            x1, y1, x2, y2 = self._tlwh_to_xyxy(box)
             track_id = track.track_id
-            outputs.append(np.array([x1,y1,x2,y2,track_id], dtype=np.int))
+            outputs.append(np.array([x1, y1, x2, y2, track_id], dtype=np.int))
         if len(outputs) > 0:
-            outputs = np.stack(outputs,axis=0)
+            outputs = np.stack(outputs, axis=0)
         return outputs
-
 
     """
     TODO:
@@ -65,18 +64,17 @@ class DeepSort(object):
             bbox_tlwh = bbox_xywh.copy()
         elif isinstance(bbox_xywh, torch.Tensor):
             bbox_tlwh = bbox_xywh.clone()
-        bbox_tlwh[:,0] = bbox_xywh[:,0] - bbox_xywh[:,2]/2.
-        bbox_tlwh[:,1] = bbox_xywh[:,1] - bbox_xywh[:,3]/2.
+        bbox_tlwh[:, 0] = bbox_xywh[:, 0] - bbox_xywh[:, 2] / 2.
+        bbox_tlwh[:, 1] = bbox_xywh[:, 1] - bbox_xywh[:, 3] / 2.
         return bbox_tlwh
 
-
     def _xywh_to_xyxy(self, bbox_xywh):
-        x,y,w,h = bbox_xywh
-        x1 = max(int(x-w/2),0)
-        x2 = min(int(x+w/2),self.width-1)
-        y1 = max(int(y-h/2),0)
-        y2 = min(int(y+h/2),self.height-1)
-        return x1,y1,x2,y2
+        x, y, w, h = bbox_xywh
+        x1 = max(int(x - w / 2), 0)
+        x2 = min(int(x + w / 2), self.width - 1)
+        y1 = max(int(y - h / 2), 0)
+        y2 = min(int(y + h / 2), self.height - 1)
+        return x1, y1, x2, y2
 
     def _tlwh_to_xyxy(self, bbox_tlwh):
         """
@@ -84,23 +82,21 @@ class DeepSort(object):
             Convert bbox from xtl_ytl_w_h to xc_yc_w_h
         Thanks JieChen91@github.com for reporting this bug!
         """
-        x,y,w,h = bbox_tlwh
-        x1 = max(int(x),0)
-        x2 = min(int(x+w),self.width-1)
-        y1 = max(int(y),0)
-        y2 = min(int(y+h),self.height-1)
-        return x1,y1,x2,y2
-    
+        x, y, w, h = bbox_tlwh
+        x1 = max(int(x), 0)
+        x2 = min(int(x + w), self.width - 1)
+        y1 = max(int(y), 0)
+        y2 = min(int(y + h), self.height - 1)
+        return x1, y1, x2, y2
+
     def _get_features(self, bbox_xywh, ori_img):
         im_crops = []
         for box in bbox_xywh:
-            x1,y1,x2,y2 = self._xywh_to_xyxy(box)
-            im = ori_img[y1:y2,x1:x2]
+            x1, y1, x2, y2 = self._xywh_to_xyxy(box)
+            im = ori_img[y1:y2, x1:x2]
             im_crops.append(im)
         if im_crops:
             features = self.extractor(im_crops)
         else:
             features = np.array([])
         return features
-
-
